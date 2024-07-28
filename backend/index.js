23aa26daa6aad019bd9eb221f5172af637219d26
adithya_narisipilli <narisipilliadithya1010@gmail.com>
2024-07-28 12:08:24 +0530

    final changes for deployment

--- backend/index.js
+++ backend/index.js
@@ -2,19 +2,9 @@ import express from 'express';
 import DBConnection from './database/db.js';
 import router from './routes/routes.js';
 import cors from 'cors';
-import path from 'path';
-
-
-const __dirname = path.resolve();
 
 const app=express();
 
-
-app.use(express.static(path.join(__dirname, '/client/dist')));
-
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
-});
 //middlewares
 app.use(cors());
 app.use(express.json());
