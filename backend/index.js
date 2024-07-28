e4f8e473c9f0c92f8cfbc3ddf89f5b4aa406169a
adithya_narisipilli <narisipilliadithya1010@gmail.com>
2024-07-28 22:34:26 +0530

    test

--- backend/index.js
+++ backend/index.js
@@ -2,8 +2,15 @@ import express from 'express';
 import DBConnection from './database/db.js';
 import router from './routes/routes.js';
 import cors from 'cors';
-
+import path from 'path';
+const __dirname = path.resolve();
 const app=express();
+
+app.use(express.static(path.join(__dirname, '../frontend/dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'frontend', 'dist', 'index.html'));
+});
 
 //middlewares
 app.use(cors());
