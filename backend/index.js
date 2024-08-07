7700442dce001f6295ad5abe43d4649a9eb731ef
adithya_narisipilli <narisipilliadithya1010@gmail.com>
2024-08-07 22:58:01 +0530

    ..

--- backend/index.js
+++ backend/index.js
@@ -8,11 +8,7 @@ const __dirname = path.resolve();
 
 const app=express();
 
-app.use(express.static(path.join(__dirname, '/frontend/dist')));
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'frontend', 'dist', 'index.html'));
-});
 
 
 
